a527ed3080007e31d5b70aa9d6f7faec73ecc531
jsvigneshkanna <vkrishitaran@gmail.com>
2025-08-10 16:07:29 +0530

    add sample class ruby code

--- class.rb
+++ class.rb
@@ -1,2 +1,25 @@
-# frozen_string_literal: true
+class Person
+  attr_accessor :name, :age, :gender
 
+  def initialize(name)
+    @name = name
+  end
+
+  def add_age(age)
+    @age = age
+  end
+
+  def add_gender(gender)
+    @gender = gender
+  end
+
+  def greet
+    puts "Hello, my name is #{@name}"
+  end
+end
+
+
+person = Person.new("John")
+person.add_age(25)
+person.add_gender("male")
+person.greet
